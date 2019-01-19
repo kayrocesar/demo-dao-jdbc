2a3e22d078b8bc5bbfb924210e53429015d8e3d4
kayrocesar <kayro.cesar.kc@gmail.com>
2019-01-19 21:57:53 -0200

    Adicionado findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,10 @@
 package application;
 
-import java.util.Date;
 
-import model.entities.Department;
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+
 import model.entities.Seller;
 
 public class Program {
@@ -11,9 +13,9 @@ public class Program {
 
 		
 		
-		Department obj= new Department(1, "Books");
+		SellerDao sellerDao= DaoFactory.createSellerDao();
 		
-		Seller seller= new Seller(21, "Bob", "Bob@gmail.com", new Date(),3000.0, obj);
+		Seller seller =sellerDao.findById(3);
 		
 		System.out.println(seller);
 	}
