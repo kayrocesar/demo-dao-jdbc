912c3ee520923097e05ec8ff13d16e2a304814d7
kayrocesar <kayro.cesar.kc@gmail.com>
2019-01-19 22:36:23 -0200

    Busca por departamento adicionada

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,9 +2,11 @@ package application;
 
 
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -12,12 +14,22 @@ public class Program {
 	public static void main(String[] args) {
 
 		
-		
+		System.out.println("------------TEST 1 : SELLER  findById--------------");
 		SellerDao sellerDao= DaoFactory.createSellerDao();
 		
 		Seller seller =sellerDao.findById(3);
 		
 		System.out.println(seller);
+		
+		System.out.println("\n------------TEST 2 : SELLER  findByDepartment--------------");
+		
+		Department department= new Department(2, null);
+		List<Seller> list=sellerDao.findByDepartment(department);
+		
+		for(Seller obj : list) {
+			
+			System.out.println(obj);
+		}
 	}
 
 }
